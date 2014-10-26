2e5add4aa674223eabffc0a91fa83aaf28746bd9
chang47 <coralbue@gmail.com>
2014-10-26 12:48:37 -0700

    asynchronous issues with SpookyJS

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,35 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Tweet Generator' });
 });
 
+router.post('/result', function(req, res) {
+	var urlTitle;
+	var Spooky = require('spooky');
+	var casper = new Spooky({
+        child: {
+            transport: 'http'
+        },
+        casper: {
+            logLevel: 'debug',
+            verbose: true
+        }
+    }, function (err) {
+        if (err) {
+            e = new Error('Failed to initialize SpookyJS');
+            e.details = err;
+            throw e;
+        }
+
+        casper.start('http://en.wikipedia.org/wiki/Web_scraping', function() {
+	    	urlTitle = this.echo("I'm loaded.");
+		});
+		casper.run();
+	});
+	var num = 0;
+	for(var i = 0; i < 1000000; i++) {
+		num++;
+	}
+	res.render('result', { title: num})
+	
+});
+
 module.exports = router;
--- spookytest.js
+++ spookytest.js
@@ -1,25 +1,7 @@
-var spooky = new Spooky({
-  casper: {
-    //configure casperjs here
-  }
-}, function (err) {
-  // NODE CONTEXT
-  console.log('We are in the Node context');
-  spooky.start('http://www.mysite.com/');
-  spooky.then(function() {
-    // CASPERJS CONTEXT
-    console.log('We are in the CasperJS context');
-    this.emit('console', 'We can also emit events here.');
-    this.click('a#somelink');
-  });
-  spooky.then(function() {
-    // CASPERJS CONTEXT
-    var size = this.evaluate(function() {
-    // PAGE CONTEXT
-    console.log('....'); // DOES NOT GET PRINTED OUT
-    __utils__.echo('We are in the Page context'); // Gets printed out
-    this.capture('screenshot.png');
-    var $selectsize = $('select#myselectlist option').size();
-      return $selectsize;
-    })
-  })
+var casper = require('casper').create();
+
+casper.start('http://en.wikipedia.org/wiki/Web_scraping', function() {
+    this.echo("I'm loaded.");
+});
+
+casper.run();
