f823f0e9b68300f3bd6fe568cb9c4834a41bac8f
chang47 <coralbue@gmail.com>
2014-11-06 19:51:33 -0800

    scraped content

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var request = require('request');
+var cheerio = require('cheerio');
 
 
 var routes = require('./routes/index');
@@ -74,11 +75,55 @@ app.get('/add', function(req, res) {
     res.render('add', { title: "Add new Data Point"});
 });
 
+app.get('/test', function(req, res) {
+    var db = req.db
+    var url = req.body.url
+    var collection = db.get('usercollection');
+    //var userTitle = scrape(url, db)
+    var val = "";
+    request('http://news.ycombinator.com', function(error, response, html) {
+        if(!error && response.statusCode == 200) {
+            //console.log(html);
+            var $ = cheerio.load(html);
+            $('span.comhead').each(function(i, element) {
+                var a = $(this).prev();
+                var rank = a.parent().parent().text();
+                var title = a.text();
+                var url = a.attr('href');
+                var subtext = a.parent().parent().next().children('.subtext').children();
+                var points = $(subtext).eq(0).text();
+                var username = $(subtext).eq(1).text();
+                var comments = $(subtext).eq(2).text();
+
+                var metadata = {
+                    rank: parseInt(rank),
+                    title: title,
+                    url: url,
+                    points: parseInt(points),
+                    username: username,
+                    comments: parseInt(comments)
+                }
+                collection.insert(metadata, function(err, doc) {
+                    if(err) {
+                        console.log("error");
+                    } else {
+                        console.log("it worked!");
+                    }
+                });
+                console.log(metadata);
+            });
+        }
+    });
+    res.send("nice try");
+});
+
 app.post('/added', function(req, res) {
     var db = req.db
     var url = req.body.url
+    var collection = db.get('usercollection');
+    //var userTitle = scrape(url, db)
+   
 
-    var userTitle = scrape(url, db)
 
     res.location('/result')
     res.redirect('/result')
