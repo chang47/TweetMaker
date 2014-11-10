2e463374c2cd62a0139f40f262bf7520513eb3a0
chang47 <coralbue@gmail.com>
2014-11-10 07:46:56 -0800

    added test user add and delete

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,8 +1,10 @@
 var userListData = [];
 $(document).ready(function() {
 	populateTable();
-	$('#userList table tbody').on('click', 'td a.linkshowuser', showerUserInfo);
-})
+	$('#userList table tbody').on('click', 'td a.linkshowuser', showUserInfo);
+	$('#btnAddUser').on('click', addUser);
+	$('#userList table tbody').on('click', 'td a.linkdeleteuser', deleteUser);
+});
 
 function populateTable() {
 	var tableContent = "";
@@ -32,4 +34,64 @@ function showUserInfo(event) {
 	$('#userInfoGender').text(thisUserObject.gender);
 	$('#userInfoLocation').text(thisUserObject.location);
 
+};
+
+function addUser(event) {
+	event.preventDefault();
+	var errorCount = 0;
+	$('#addUser input').each(function(index, val) {
+		if($(this).val() === '') {
+			errorCount++;
+		}
+	});
+	if(errorCount === 0) {
+		var newUser = {
+			'username': $('#addUser fieldset input#inputUserName').val(),
+			'email': $('#addUser fieldset input#inputUserEmail').val(),
+			'fullname': $('#addUser fieldset input#inputUserFullName').val(),
+			'age': $('#addUser fieldset input#inputUserAge').val(),
+			'location': $('#addUser fieldset input#inputUserLocation').val(),
+			'gender': $('#addUser fieldset input#inputUserGender').val()
+		}
+
+		$.ajax({
+			type: 'Post',
+			data: newUser,
+			url: '/users/adduser',
+			dataType: 'JSON'
+		}).done(function(response) {
+			if(response.msg === '') {
+				$('#addUser fieldset input').val('');
+				populateTable();
+			} else {
+				alert('Error: ' + response.msg);
+			}
+		});
+	} else {
+		alert('Please fill in all fields');
+		return false;
+	}
+
+
+};
+
+function deleteUser(event) {
+	event.preventDefault();
+	var confirmation = confirm("Are you sure you want to delete this user?");
+
+	if(confirmation === true) {
+		$.ajax({
+			type: 'DELETE',
+			url: '/users/deleteuser/' + $(this).attr('rel')
+		}).done(function(response) {
+			if (response.msg === '') {
+            }
+            else {
+                alert('Error: ' + response.msg);
+            }
+			populateTable();
+		});
+	} else {
+		return false;
+	}
 };
