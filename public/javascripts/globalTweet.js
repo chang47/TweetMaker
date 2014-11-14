3b03a78d68beb6eaba13a4fbe89dd226b24bbd04
chang47 <coralbue@gmail.com>
2014-11-14 07:36:10 -0800

    added word counter

--- public/javascripts/globalTweet.js
+++ public/javascripts/globalTweet.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 	$('#btnSubmit').on('click', searchTweets);
+	$('.results').on('click', editKeyboard);
 });
 
 //@TODO parse string to be 118 char and include URL
@@ -26,9 +27,18 @@ function populateTweets(json) {
 	for(var key in json) {
 		content += '<div class="row">';
 		content += '<div class="col-md-6 col-md-offset-0">'
-		content += '<textarea class="form-control" rows="4">' + json[key] + '</textarea>'
-		content += '</div></div>';
+		content += '<textarea class="form-control results" rows="4">' + json[key] + '</textarea>'
+		content += '</div><span>'
+		content += 140 - json[key].length + ' characters left</span>'
+		content += '</div>';
 		content += '<p>';
 	}
 	$('#results').html(content);
+}
+
+function editKeyboard(event) {
+	event.preventDefault();
+	console.log($(this).next().text());
+	var words = $(this).parent().parent().find('span').text();
+	$(this).next().html('sdf');
 }
