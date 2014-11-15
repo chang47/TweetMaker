c2ddff6b21cf562c83604d9b590d5b5a53d2e12c
chang47 <coralbue@gmail.com>
2014-11-15 09:20:44 -0800

    parsing data to submit to buffer

--- public/javascripts/globalTweet.js
+++ public/javascripts/globalTweet.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 	$('#btnSubmit').on('click', searchTweets);
 	$('.results').on('click', editKeyboard);
+	$(document).on('click', '#send', sendCall);
 });
 
 //@TODO parse string to be 118 char and include URL
@@ -27,18 +28,28 @@ function populateTweets(json) {
 	for(var key in json) {
 		content += '<div class="row">';
 		content += '<div class="col-md-6 col-md-offset-0">'
-		content += '<textarea class="form-control results" rows="4">' + json[key] + '</textarea>'
+		content += '<textarea class="form-control results" rows="3">' + json[key] + '</textarea>'
 		content += '</div><span>'
-		content += 140 - json[key].length + ' characters left</span>'
+		content += 140 - json[key].length + ' characters left<br /><input type="checkbox"> <b>Add this URL</b></span>'
 		content += '</div>';
 		content += '<p>';
 	}
+	content += '<button id="send" type="submit">Submit</button>';
 	$('#results').html(content);
 }
 
+//@TODO doesn't work
 function editKeyboard(event) {
 	event.preventDefault();
 	console.log($(this).next().text());
 	var words = $(this).parent().parent().find('span').text();
 	$(this).next().html('sdf');
 }
+
+function sendCall() {
+	$('#results').children().each(function(key) {
+		console.log($(this).find('textarea').text());
+	});
+} 
+
+
