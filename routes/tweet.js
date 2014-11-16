12fc6a6487f75ba236a42ec7f4a4ee5bfd649b24
chang47 <coralbue@gmail.com>
2014-11-16 10:37:18 -0800

    registering with twitter api

--- routes/tweet.js
+++ routes/tweet.js
@@ -2,9 +2,10 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 var cheerio = require('cheerio');
+var OAuth = require('OAuth');
 
 router.post('/call', function(req, res) {
-    var url = "buffer api";
+    var url = "https://api.bufferapp.com/1/profiles.json&pretty=true";
     var tweets = req.body; //@TODO what does this grab?
     console.log(tweets);
 /*    request(url, function(error, response, html) {
@@ -42,6 +43,43 @@ router.post('/maketweets', function(req, res) {
     });
 });
 
+router.get('/done', function(req, res) {
+    res.send("Your tweets have been successfully added");
+});
+
+router.get('/access', function(req, res) {
+    var OAuth2 = OAuth.OAuth2;
+    var twitterConsumerKey = '';
+    var twitterConsumerSecret = '';
+    var oauth2 = new OAuth2(
+        twitterConsumerKey,
+        twitterConsumerSecret,
+        'http://api.twitter.com/',
+        null,
+        'oauth2/token',
+        null);
+    oauth2.getOAuthAccessToken(
+        '',
+        {'grant_type': 'client_credentials'},
+        function(e, access_token, refresh_token, results) {
+            console.log('bearer: ', access_token);
+            oauth2.get('protected url',
+                access_token, function(e, data, res) {
+                    if (e) return callback(e, null);
+                    if (res.statusCode != 200) {
+                        return callback(new Error(
+                            'OAuth2 request failed: ' +
+                            res.statusCode), null);
+                    } 
+                    try {
+                        data = JSON.parse(data);
+                    } catch(e) {
+                        return callback(e, null);
+                    }
+                    return callback(e, data);
+                });
+        });
+});
 
 
 module.exports = router;
