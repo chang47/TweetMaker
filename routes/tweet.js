b6bc37b8415fb183e3b3c966afc312ce6ecf8477
chang47 <coralbue@gmail.com>
2014-11-18 07:45:34 -0800

    exploring oauth controls

--- routes/tweet.js
+++ routes/tweet.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 var cheerio = require('cheerio');
-//var OAuth = require('OAuth');
+var OAuth = require('oauth');
 
 router.post('/call', function(req, res) {
     var url = "https://api.bufferapp.com/1/profiles.json&pretty=true";
@@ -47,39 +47,12 @@ router.get('/done', function(req, res) {
     res.send("Your tweets have been successfully added");
 });
 
-/*router.get('/access', function(req, res) {
+router.get('/access', function(req, res) {
     var OAuth2 = OAuth.OAuth2;
     var twitterConsumerKey = '';
     var twitterConsumerSecret = '';
-    var oauth2 = new OAuth2(
-        twitterConsumerKey,
-        twitterConsumerSecret,
-        'http://api.twitter.com/',
-        null,
-        'oauth2/token',
-        null);
-    oauth2.getOAuthAccessToken(
-        '',
-        {'grant_type': 'client_credentials'},
-        function(e, access_token, refresh_token, results) {
-            console.log('bearer: ', access_token);
-            oauth2.get('protected url',
-                access_token, function(e, data, res) {
-                    if (e) return callback(e, null);
-                    if (res.statusCode != 200) {
-                        return callback(new Error(
-                            'OAuth2 request failed: ' +
-                            res.statusCode), null);
-                    } 
-                    try {
-                        data = JSON.parse(data);
-                    } catch(e) {
-                        return callback(e, null);
-                    }
-                    return callback(e, data);
-                });
-        });
-});*/
+    
+});
 
 
 module.exports = router;
